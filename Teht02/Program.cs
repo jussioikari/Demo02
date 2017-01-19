b7dda1775b03ab14185220bf4d5033c27f505e0b
L1628 <L1628@OHJE-330-WS-27>
2017-01-19 13:54:13 +0200

    teht2

---
+++ Teht02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points;
+            //Kysy pisteitä
+            Console.WriteLine("Give points");
+            points = int.Parse(Console.ReadLine());
+            if (points <= 1) Console.WriteLine("Koulunumero on 0");
+            else if (points == 2 || points == 3) Console.WriteLine("Koulunumero on 1");
+            else if (points == 4 || points == 5) Console.WriteLine("Koulunumero on 2");
+            else if (points == 6 || points == 7) Console.WriteLine("Koulunumero on 3");
+            else if (points == 8 || points == 9) Console.WriteLine("Koulunumero on 4");
+            else if (points == 10 || points == 11 || points == 12) Console.WriteLine("Koulunumero on 5");
+            else if (points >12) Console.WriteLine("Ei pisterajoissa");
+
+        }
+    }
+}
