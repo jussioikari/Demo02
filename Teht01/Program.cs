1ec7c6afd79b20d9d509c2d477dacaf1de68eac0
L1628 <L1628@OHJE-330-WS-27>
2017-01-19 13:37:46 +0200

    teht1

---
+++ Teht01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+            Console.WriteLine("Anna luku");
+            luku = int.Parse(Console.ReadLine());
+            switch (luku)
+            {
+                case 1: Console.WriteLine("Yksi"); break;
+                case 2: Console.WriteLine("Kaksi"); break;
+                case 3: Console.WriteLine("Kolme"); break;
+                default: Console.WriteLine("Joku muu luku"); break;
+            }
+
+        }
+    }
+}
