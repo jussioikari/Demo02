dcf59b18e4c0ff6d11c6604f2941e6b8c064add5
L1628 <L1628@OHJE-330-WS-27>
2017-01-19 15:05:42 +0200

    teht07

---
+++ Teht07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna vuosiluku");
+            int vuosi;
+            vuosi = int.Parse(Console.ReadLine());
+
+            if (vuosi % 4 == 0 && vuosi % 100 != 0 || vuosi % 400 == 0)
+            {
+                Console.WriteLine("Karkausvuosi");
+
+            }
+            else
+            {
+                Console.WriteLine("Ei ole karkausvuosi");
+            }
+        }
+    }
+}
