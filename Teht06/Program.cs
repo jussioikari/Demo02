5106ee3f0e2ea5df75b346fb4ff72edab7389036
L1628 <L1628@OHJE-330-WS-27>
2017-01-19 14:47:43 +0200

    teht06

---
+++ Teht06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna km");
+            double km;
+            km = double.Parse(Console.ReadLine());
+
+            double bensa = km * 0.0702;
+            double raha = bensa * 1.595;
+            Console.WriteLine("Bensaa kuluu " + bensa + " litraa, " + "kustannus " + raha + " euroa" );
+        }
+    }
+}
